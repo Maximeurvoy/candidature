94c0710dd2dd5082faaa2e8d38915cdcc326ee12
Maximeurvoy <maxime.ptitx@gmail.com>
2019-12-11 23:45:33 +0100

    add icon linkedin and now go to bed

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,6 +7,7 @@ const Contact = () => (
     <p>06.85.12.16.56</p>
     <p><a href={('/Maxime_Urvoy.pdf')}
    download="Maxime_Urvoy.pdf">Télécharger mon CV</a></p>
+    <a href='https://www.linkedin.com/in/maxime-urvoy-093577196/' target="_blank" rel="noopener noreferrer"><img className='icon'  src={('/linkedin.png')} alt='linkedin icon'/></a>
   </>
 );
 
